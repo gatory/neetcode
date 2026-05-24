543787476a689210c273cddf57bd104d64c7916a
Kuan Wei <rueikuanwei@gmail.com>
2026-05-23 17:18:34 -0700

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,25 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timeMap = defaultdict(list) 
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timeMap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.timeMap.get(key, [])
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if values[mid][0] == timestamp:
+                return values[mid][1]
+
+            if values[mid][0] < timestamp:
+                res = values[mid][1]
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return res
